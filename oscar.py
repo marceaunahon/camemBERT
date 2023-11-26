8df8fe542a707cc00eb410883e0c2030a2d5ff1c
SPI-2020-70 <maximecha14@gamil.com>
2023-11-26 14:08:38 +0100

    tokenizaton v1

--- oscar.py
+++ oscar.py
@@ -2,6 +2,9 @@ from typing import Any
 from datasets import load_dataset
 import random
 import re
+import nltk
+from nltk.tokenize import word_tokenize
+from collections import Counter
 
 
 class Oscar():
@@ -15,6 +18,7 @@ class Oscar():
                         use_auth_token="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", # required
                         language=language,
                         split=split) # optional, but the dataset only has a train split
+        self.tokenized_dataset = self.tokenize_dataset()
 
     # return the element at the index of the dataset
     def __getitem__(self, index: int) -> Any:
@@ -36,11 +40,46 @@ class Oscar():
         return results
 
     # tokenize the text with the WWM method 
-    def tokenize_text(self, text: str) -> Any:
+    """     def tokenize_text(self, text: str) -> Any:
         # Tokenize the sentence
         words = re.findall(r'\b\w+\b', text)
 
         # Apply whole-word masking (WWM)
         masked_sentence = ' '.join(['[MASK]' if random.random() < 0.15 else word for word in words])
 
-        return masked_sentence
+        return masked_sentence """
+
+    def tokenize_text(self, text: str) -> Any:
+        # Tokenize the sentence
+        tokens = word_tokenize(text)
+
+        return tokens
+    
+    def tokenize_dataset(self) -> Any:
+        try:
+            # Try to find the 'punkt' package
+            nltk.data.find('tokenizers/punkt')
+            print("'punkt' is already installed.")
+        except LookupError:
+            # If not found, download it
+            nltk.download('punkt')
+            print("'punkt' has been downloaded.")
+        # Tokenize the dataset
+        # create an empty list of the same size as the dataset
+        tokenized_dataset = [None] * len(self.dataset)
+        for i,sentence in enumerate(self.dataset):
+            if i % 1000 == 0:
+                print(f"\rTokenizing {i}/{len(self.dataset)}", end="")
+            tokenized_dataset[i] = self.tokenize_text(sentence["text"])
+
+        print(f"\rTokenized {len(self.dataset)}/{len(self.dataset)} sentences.")
+
+        return tokenized_dataset
+    
+    def get_vocab(self) -> Any:
+        vocab = Counter()
+
+        for sentence in self.tokenized_dataset:
+            vocab.update(sentence)
+
+        return vocab
