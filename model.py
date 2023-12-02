708099ea896c44d8110462e7c4830449be051cd0
marceaunahon <marceau.nahon@gmail.com>
2023-12-02 16:01:52 +0100

    d_ffn added

--- model.py
+++ model.py
@@ -4,16 +4,20 @@ from typing import Tuple, List
 
 
 class Transformer(nn.Module):
-    def __init__(self, dictionary: List[str], max_seq_len : int=512, d_model : int = 768, num_heads : int = 12, num_layers : int = 6, dropout : float = 0.1) -> None:
+    def __init__(self, dictionary: List[str], max_seq_len : int=512, d_model : int = 768, num_heads : int = 12, num_layers : int = 6, d_ffn : int = 0, dropout : float = 0.1) -> None:
         super().__init__()
         self.dictionary = dictionary
         self.d_model = d_model
         self.num_heads = num_heads
         self.num_layers = num_layers
+        if d_ffn == 0: # de manière générale, d_ffn vaut 4 * d_model
+            self.d_ffn = 4 * self.d_model
+        else:
+            self.d_ffn = d_ffn
         self.dropout = dropout
         self.positional_encoding = PositionalEncoding(d_model = self.d_model, max_seq_len = max_seq_len)
-        self.encoder = Encoder(d_model = self.d_model, num_heads = self.num_heads, num_layers = self.num_layers, dropout = self.dropout)
-        self.decoder = Decoder(d_model = self.d_model, num_heads = self.num_heads, num_layers = self.num_layers, dropout = self.dropout)
+        self.encoder = Encoder(d_model = self.d_model, num_heads = self.num_heads, num_layers = self.num_layers, d_ffn= self.d_ffn, dropout = self.dropout)
+        self.decoder = Decoder(d_model = self.d_model, num_heads = self.num_heads, num_layers = self.num_layers, d_ffn = self.d_ffn, dropout = self.dropout)
         self.linear = nn.Linear(in_features = self.d_model, out_features = len(self.dictionary)) 
         
 
@@ -51,13 +55,14 @@ class PositionalEncoding(nn.Module):
         return encoded_x
     
 class Encoder(nn.Module):  
-    def __init__(self, d_model : int = 768, num_heads : int = 12 , num_layers : int = 6, dropout : float = 0.1) -> None:
+    def __init__(self, d_model : int, num_heads : int, num_layers : int, d_ffn : int, dropout : float) -> None:
         super().__init__()
         self.d_model = d_model
         self.num_heads = num_heads
         self.num_layers = num_layers
+        self.d_ffn = d_ffn
         self.dropout = dropout
-        self.encoder_layers = nn.ModuleList([EncoderLayer(d_model, num_heads, dropout) for _ in range(num_layers)])
+        self.encoder_layers = nn.ModuleList([EncoderLayer(self.d_model, self.num_heads, self.d_ffn, self.dropout) for _ in range(num_layers)])
 
     def forward(self, x : torch.Tensor) -> torch.Tensor:
         for encoder_layer in self.encoder_layers:
@@ -65,13 +70,14 @@ class Encoder(nn.Module):
         return x
 
 class EncoderLayer(nn.Module):
-    def __init__(self, d_model : int, num_heads : int, dropout : float) -> None:
+    def __init__(self, d_model : int, num_heads : int, d_ffn : int, dropout : float) -> None:
         super().__init__()
         self.d_model = d_model
         self.num_heads = num_heads
+        self.d_ffn = d_ffn
         self.dropout = dropout
         self.multihead_attention_layer = MultiHeadAttentionSubLayer(d_model = self.d_model, num_heads = self.num_heads, dropout = self.dropout)
-        self.position_wise_fully_connected_feed_forward_layer = PositionWiseFullyConnectedFeedForwardSubLayer(d_model = self.d_model, dropout = self.dropout)
+        self.position_wise_fully_connected_feed_forward_layer = PositionWiseFullyConnectedFeedForwardSubLayer(d_model = self.d_model, dropout = self.dropout, d_ffn = self.d_ffn)
 
     def forward(self, x : torch.Tensor) -> torch.Tensor:
         attn_output, attn_output_weights = self.multihead_attention_layer(x, x, x) #faut voir ce truc la, automatiquement copilot met x,x,x c bizarre
@@ -97,14 +103,11 @@ class MultiHeadAttentionSubLayer(nn.Module):
         return attn_output, attn_output_weights
 
 class PositionWiseFullyConnectedFeedForwardSubLayer(nn.Module):
-    def __init__(self, d_model : int, dropout : float, d_ffn : int = 0) -> None:
+    def __init__(self, d_model : int, dropout : float, d_ffn : int) -> None:
         super().__init__()
         self.d_model = d_model
         self.dropout = dropout
-        if d_ffn == 0: # de manière générale, d_ffn vaut 4 * d_model
-            self.d_ffn = 4 * self.d_model
-        else:
-            self.d_ffn = d_ffn
+        self.d_ffn = d_ffn
         self.feed_forward = nn.Sequential(
             nn.Linear(self.d_model, self.d_ffn),
             nn.GELU(),
@@ -120,14 +123,15 @@ class PositionWiseFullyConnectedFeedForwardSubLayer(nn.Module):
     
 
 class Decoder(nn.Module):
-    def __init__(self, d_model : int = 768, num_heads : int = 12 , num_layers : int = 6, dropout : float = 0.1, encoder_output : torch.Tensor = torch.Tensor()) -> None:
+    def __init__(self, d_model : int, num_heads : int, num_layers : int, d_ffn : int, dropout : float, encoder_output : torch.Tensor) -> None:
         super().__init__()
         self.d_model = d_model
         self.num_heads = num_heads
         self.num_layers = num_layers
+        self.d_ffn = d_ffn
         self.dropout = dropout
         self.encoder_output = encoder_output
-        self.decoder_layers = nn.ModuleList([DecoderLayer(self.d_model, self.num_heads, self.dropout) for _ in range(num_layers)])
+        self.decoder_layers = nn.ModuleList([DecoderLayer(self.d_model, self.num_heads, self.d_ffn, self.dropout) for _ in range(num_layers)])
 
     def forward(self, encoder_ouptut : torch.Tensor, output_embedding : torch.Tensor) -> torch.Tensor:
         x = torch.Tensor()
@@ -136,14 +140,15 @@ class Decoder(nn.Module):
         return x
     
 class DecoderLayer(nn.Module):
-    def __init__(self, d_model : int, num_heads : int, dropout : float) -> None:
+    def __init__(self, d_model : int, num_heads : int, d_ffn : int, dropout : float) -> None:
         super().__init__()
         self.d_model = d_model
         self.num_heads = num_heads
         self.dropout = dropout
+        self.d_ffn = d_ffn
         self.mask_multihead_attention_layer = MultiHeadAttentionSubLayer(d_model = self.d_model, num_heads = self.num_heads, dropout = self.dropout) #voir la diff entre les deux attention
         self.multihead_attention_layer = MultiHeadAttentionSubLayer(d_model = self.d_model, num_heads = self.num_heads, dropout = self.dropout)
-        self.position_wise_fully_connected_feed_forward_layer = PositionWiseFullyConnectedFeedForwardSubLayer(d_model = self.d_model, dropout = self.dropout)
+        self.position_wise_fully_connected_feed_forward_layer = PositionWiseFullyConnectedFeedForwardSubLayer(d_model = self.d_model, d_ffn = d_ffn, dropout = self.dropout)
 
     def forward(self, encoder_output : torch.Tensor, output_embedding : torch.Tensor) -> torch.Tensor:
         attn_output, attn_output_weights = self.mask_multihead_attention_layer(output_embedding, output_embedding, output_embedding)
