9d99173fe8bbf85a99122593764c926643562d92
mayarabdelgawad <mayar.abdelgawad_shehab@etu.sorbonne-universite.fr>
2023-12-05 20:54:30 +0100

    update train

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import StepLR
 from tqdm import tqdm
 
-# Import your Oscar class and Transformer model
+# Import Oscar class and Transformer model
 from oscar import Oscar
 from model import Transformer
 
@@ -18,6 +19,9 @@ model = Transformer(dictionary)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+# Learning rate scheduler
+scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
+
 # Set device (CPU/GPU)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
@@ -27,9 +31,12 @@ batch_size = 32  # Change this to whatever fits in our GPU
 dataloader = DataLoader(range(len(oscar)), batch_size=batch_size, shuffle=True)
 
 # Training loop
-num_epochs = 5 # Change this to if we want to train for more epochs
+num_epochs = 5 # Change this too if we want to train for more epochs
+best_loss = float('inf')
+patience, trials = 10, 0
 for epoch in range(num_epochs):
     total_loss = 0
+    model.train()
     for batch_idx in tqdm(dataloader, desc=f"Epoch {epoch + 1}/{num_epochs}"):
         # Get batch data from the Oscar dataset
         inputs = [oscar[i] for i in batch_idx]
@@ -39,7 +46,7 @@ for epoch in range(num_epochs):
         outputs = model(inputs, output_encoding=None)  # Assuming output_encoding is None for training
 
         # Calculate loss
-        targets = torch.randint(0, len(dictionary), (batch_size,)).to(device)
+        targets = torch.randint(0, len(dictionary), (batch_size,)).to(device)  # Replace this with correct targets
         loss = criterion(outputs, targets)
 
         # Backward pass and optimization
@@ -49,9 +56,19 @@ for epoch in range(num_epochs):
 
         total_loss += loss.item()
 
+    scheduler.step()
+
     # Print average loss for the epoch
     avg_loss = total_loss / len(dataloader)
     print(f"Epoch {epoch + 1}/{num_epochs}, Average Loss: {avg_loss}")
 
-# Save the trained model
-torch.save(model.state_dict(), "camembert_model.pth")
+    # Early stopping
+    if avg_loss < best_loss:
+        trials = 0
+        best_loss = avg_loss
+        torch.save(model.state_dict(), "camembert_model.pth")
+    else:
+        trials += 1
+        if trials >= patience:
+            print(f'Early stopping on epoch {epoch}')
+            break
