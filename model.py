277b093442d4e619048ef7ceeb805ede3a91fc24
marceaunahon <marceau.nahon@gmail.com>
2023-12-15 18:28:55 +0100

    attn_mask added

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from torch import nn
 import torch
-from typing import Tuple, List
+from typing import Tuple, List, Any
 
 import torch
 from torch import nn, optim
@@ -9,9 +9,11 @@ from torch.optim.lr_scheduler import StepLR
 from tqdm import tqdm
 from oscar import Oscar
 
+import numpy as np
+
 
 class Transformer(nn.Module):
-    def __init__(self, dictionary: List[str], max_seq_len : int=200, d_model : int = 512, num_heads : int = 8, num_layers : int = 6, d_ffn : int = 0, dropout : float = 0.1, start_token_id:int = 1, end_token_id:int=2) -> None:
+    def __init__(self, dictionary: List[str], max_seq_len : int=200, d_model : int = 768, num_heads : int = 8, num_layers : int = 6, d_ffn : int = 0, dropout : float = 0.1, start_token_id:int = 1, end_token_id:int=2) -> None:
         super().__init__()
         self.dictionary = dictionary
         self.d_model = d_model
@@ -30,11 +32,39 @@ class Transformer(nn.Module):
         self.linear = nn.Linear(in_features = self.d_model, out_features = len(self.dictionary)) 
         
 
-    def forward(self, input : torch.Tensor, label : torch.Tensor) -> torch.Tensor:
+    def forward(self, input : torch.Tensor, label : torch.Tensor, attn_mask : torch.Tensor) -> torch.Tensor:
         encoder_output = self.encoder(input)
-        decoder_output = self.decoder(encoder_output, label)
+        decoder_output = self.decoder(encoder_output, label, attn_mask)
         output = self.linear(decoder_output)
         return output
+    
+    def get_dictionary(self) -> List[str]:
+        return self.dictionary
+    
+    def get_d_model(self) -> int:
+        return self.d_model
+    
+    def get_num_heads(self) -> int:
+        return self.num_heads
+    
+    def get_num_layers(self) -> int:
+        return self.num_layers
+    
+    def get_max_seq_len(self) -> int:
+        return self.max_seq_len
+    
+    def get_start_token(self) -> int:
+        return self.start_token
+    
+    def get_end_token(self) -> int:
+        return self.end_token
+    
+    def get_d_ffn(self) -> int:
+        return self.d_ffn
+    
+    def get_dropout(self) -> float:
+        return self.dropout   
+
 
 class PositionalEncoding(nn.Module):
     def __init__(self, vocab_size:int, d_model:int=512, max_seq_len : int=512):
@@ -95,9 +125,7 @@ class EncoderLayer(nn.Module):
         self.position_wise_fully_connected_feed_forward_layer = PositionWiseFullyConnectedFeedForwardSubLayer(d_model = self.d_model, dropout = self.dropout, d_ffn = self.d_ffn)
 
     def forward(self, x : torch.Tensor) -> torch.Tensor:
-        # x, _ = self.multihead_attention_layer(x, x, x) #faut voir ce truc la, automatiquement copilot met x,x,x c bizarre
-        # x = self.position_wise_fully_connected_feed_forward_layer(x)
-        x = self.multihead_attention_layer(x, x, x) #faut voir ce truc la, automatiquement copilot met x,x,x c bizarre
+        x = self.multihead_attention_layer(x, x, x, None) 
         x = self.position_wise_fully_connected_feed_forward_layer(x)
         return x
    
@@ -111,8 +139,8 @@ class MultiHeadAttentionSubLayer(nn.Module):
         self.layer_norm = nn.LayerNorm(self.d_model)
 
     #Ducoup la faut voir ce que c'est query, key et value, c'est trop bien copilot autocompile mes doutes il est trop fort
-    def forward(self, query : torch.Tensor, key : torch.Tensor, value : torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        x, _ = self.multihead_attention(query, key, value)
+    def forward(self, query : torch.Tensor, key : torch.Tensor, value : torch.Tensor, attn_mask : Any) -> Tuple[torch.Tensor, torch.Tensor]:
+        x, _ = self.multihead_attention(query, key, value, attn_mask)
         x = self.layer_norm(query + x)
         return x
 
@@ -148,10 +176,10 @@ class Decoder(nn.Module):
         self.positional_encoding = PositionalEncoding(vocab_size, d_model = self.d_model, max_seq_len = self.max_seq_len)
         self.decoder_layers = nn.ModuleList([DecoderLayer(self.d_model, self.num_heads, self.d_ffn, self.dropout) for _ in range(num_layers)])
 
-    def forward(self, encoder_output : torch.Tensor, label : torch.Tensor) -> torch.Tensor:
+    def forward(self, encoder_output : torch.Tensor, label : torch.Tensor, attn_mask : torch.Tensor) -> torch.Tensor:
         label = self.positional_encoding(label)
         for decoder_layer in self.decoder_layers:
-            x = decoder_layer(encoder_output, label)
+            x = decoder_layer(encoder_output, label, attn_mask)
         return x
     
 class DecoderLayer(nn.Module):
@@ -165,16 +193,15 @@ class DecoderLayer(nn.Module):
         self.multihead_attention_layer = MultiHeadAttentionSubLayer(d_model = self.d_model, num_heads = self.num_heads, dropout = self.dropout)
         self.position_wise_fully_connected_feed_forward_layer = PositionWiseFullyConnectedFeedForwardSubLayer(d_model = self.d_model, d_ffn = d_ffn, dropout = self.dropout)
 
-    def forward(self, encoder_output, decoder_input):
+    def forward(self, encoder_output : torch.Tensor, decoder_input : torch.Tensor, attn_mask : torch.Tensor) -> torch.Tensor:
     
-        x = self.mask_multihead_attention_layer(decoder_input, decoder_input, decoder_input)
-        x = self.multihead_attention_layer(encoder_output, encoder_output, x)
+        x = self.mask_multihead_attention_layer(decoder_input, decoder_input, decoder_input, attn_mask)
+        x = self.multihead_attention_layer(encoder_output, encoder_output, x, None)
         x = self.position_wise_fully_connected_feed_forward_layer(x)
         return x
-
     
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     # Initialize Oscar object
     oscar = Oscar(language="fr", split="train")
 
@@ -186,7 +213,7 @@ if __name__ == "__main__":
 
     # Define your loss function and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
     # Learning rate scheduler
     scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
@@ -194,39 +221,45 @@ if __name__ == "__main__":
     # Set device (CPU/GPU)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
+    criterion.to(device)
+
+    indices = list(range(0, len(oscar), 10))  # This will take every second element
+
+    # Create the subset
+    subset = torch.utils.data.Subset(oscar, indices)
 
     # Define DataLoader for Oscar dataset
-    batch_size = 32  # Change this to whatever fits in our GPU
-    dataloader = DataLoader(range(len(oscar)), batch_size=batch_size, shuffle=True)
+    batch_size = 64  # Change this to whatever fits in our GPU
+    dataloader = DataLoader(subset, batch_size=batch_size, shuffle=True, num_workers=4)
 
     # Training loop
-    num_epochs = 1 # Change this too if we want to train for more epochs
+    num_epochs = 5 # Change this too if we want to train for more epochs
     best_loss = float('inf')
     patience, trials = 10, 0
+    accumulation_steps = 4  # Change this to the number of batches to accumulate gradients
+
+    model.train()  # Ensure the model is in training mode
     for epoch in range(num_epochs):
         total_loss = 0
-        model.train()
-        for batch_idx in tqdm(dataloader, desc=f"Epoch {epoch + 1}/{num_epochs}"):
-        # Get batch data from the Oscar dataset
-            # Convert batch_idx to list of inputs
-            batch_idx = batch_idx.tolist()
-            inputs = [oscar.get_masked_item(i) for i in batch_idx]
-            inputs = torch.tensor(inputs).to(device)
-            
-            # Get the correct targets
-            targets = [oscar[i] for i in batch_idx]
+        optimizer.zero_grad()  # Reset gradients at the beginning of each epoch
+        for batch_idx, (inputs, targets) in enumerate(tqdm(dataloader, desc=f"Epoch {epoch + 1}/{num_epochs}")):
+            inputs, targets = inputs.to(device), targets.to(device)
 
+            attn_mask = torch.full([model.max_seq_len, batch_size], -np.inf)
+            attn_mask = torch.triu(attn_mask, diagonal=1).to(device)
 
             # Forward pass
-            outputs = model(inputs)
+            outputs = model(inputs, targets, attn_mask)
             
             # Calculate loss
-            loss = criterion(outputs, targets)
+            loss = criterion(outputs.view(-1, outputs.size(-1)), targets.view(-1))
+            loss = loss / accumulation_steps  # Normalize the loss because it's accumulated for multiple batches
+            loss.backward()  # Backward pass
 
-            # Backward pass and optimization
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            # Perform optimization step only after accumulating gradients for accumulation_steps batches
+            if (batch_idx + 1) % accumulation_steps == 0 or batch_idx == len(dataloader) - 1:
+                optimizer.step()
+                optimizer.zero_grad()
 
             total_loss += loss.item()
 
@@ -240,7 +273,7 @@ if __name__ == "__main__":
         if avg_loss < best_loss:
             trials = 0
             best_loss = avg_loss
-            torch.save(model.state_dict(), "camembert_model.pth")
+            torch.save(model.state_dict(), "camembert2.pth")
         else:
             trials += 1
             if trials >= patience:
